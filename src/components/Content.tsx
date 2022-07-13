67781f5ac0aceb7042ad4854387517fe211efdd7
Befrosa <bernardo.furtado@aol.com>
2022-07-12 22:33:17 -0300

    Desafio

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,27 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from "./MovieCard";
+
+export function Content({selectedGenre, movies}: any) {
+  return (
+
+    <div className="container">
+
+      <header>
+        <span className="category">Categoria: <span>{selectedGenre.title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map((movie: any) => (
+            <MovieCard 
+            key ={movie.imdbID} 
+            runtime={movie.Runtime} 
+            poster={movie.Poster} 
+            title={movie.Title} 
+            rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+      
+    </div>
+  )
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,23 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from '../components/Button';
+
+export function SideBar({genres, selectedGenreId, handleClickButton}: any) {
+  return (
+    <div>
+      <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map((genre: any) => (
+            <Button
+              key={String(genre.id)}
+              onClick={() => handleClickButton(genre.id)}
+              iconName={genre.name}
+              title={genre.title}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+    </nav>
+  </div>
+  )
 }
